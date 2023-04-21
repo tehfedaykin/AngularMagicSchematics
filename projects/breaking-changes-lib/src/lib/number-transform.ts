7accadd8e4b3c13b70d0e49910a08515ed184247
Jennifer Wadella <jennifer.wadella@gmail.com>
2023-04-21 10:40:37 -0500

    mock breaking change

--- projects/breaking-changes-lib/src/lib/number-transform.ts
+++ projects/breaking-changes-lib/src/lib/number-transform.ts
@@ -1,4 +1,5 @@
-export function breakingFunction(param1: string, param2: number) {
+//lol switched the params GLHF
+export function breakingFunction(param1: number, param2: string) {
     const numberToString = param2.toString();
     return `${param1} is a string and ${numberToString} was a number`;
 }
