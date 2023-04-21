250e78ce69706293dd88f17155846411f4e2a62a
Jennifer Wadella <jennifer.wadella@gmail.com>
2023-04-21 10:36:16 -0500

    og change

--- projects/breaking-changes-lib/src/lib/number-transform.ts
+++ projects/breaking-changes-lib/src/lib/number-transform.ts
@@ -1,4 +1,4 @@
-export function breakingFunction(param1: number, param2: string) {
+export function breakingFunction(param1: string, param2: number) {
     const numberToString = param2.toString();
     return `${param1} is a string and ${numberToString} was a number`;
 }
